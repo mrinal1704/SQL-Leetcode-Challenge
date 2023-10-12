ec4ba9c093bfc42cadb03d06c544eaa553efde5c
Minhaz Alam <minhazalam365@gmail.com>
2023-10-12 14:58:09 +0530

    [Fix} Leetcode test case
    If User names are same with different user ids, it will fail some test cases

--- Easy/Top Travellers.sql
+++ Easy/Top Travellers.sql
@@ -81,5 +81,5 @@
 Select U.name as name, coalesce(sum(R.distance),0) as travelled_distance 
 from Users U left join Rides R
 on R.user_id = U.id
-group by name
+group by U.id
 Order by travelled_distance desc, name
